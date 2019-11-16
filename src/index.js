09f4d20ade35bb5f573191265f9b67305bd78f5b
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-11-16 23:19:55 +0100

    set roboto font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './router';
+import 'typeface-roboto';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
