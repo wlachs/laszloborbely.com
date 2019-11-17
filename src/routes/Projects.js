dbbb040a98edff0732048adc309ceb1ef0a6db51
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-11-17 02:01:40 +0100

    add project view content

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import Frame from '../components/Frame';
+import ProjectCard from '../components/ProjectCard';
 
 export default function Projects(props) {
     return (
         <Frame>
-            <div>Projects</div>
+            <h1>My recent work</h1>
+            <h2>Check out my latest projects.</h2>
+            <div className='d-flex justify-content-center mt-5'>
+                <ProjectCard link='https://literature.laszloborbely.com' image='./assets/literature.png'>
+                    Literature Wizard
+                </ProjectCard>
+            </div>
         </Frame>
     );
 };
