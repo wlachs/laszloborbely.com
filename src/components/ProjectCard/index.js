073faeba70c5cccb272c2c6e78c0d5a90c15f8b2
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-11-17 02:03:10 +0100

    redundant alt attribute

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -4,7 +4,7 @@ import './index.css';
 export default function ProjectCard(props) {
     return (
         <a className='project-card d-flex flex-column justify-content-around align-items-center' href={props.link}>
-            <img src={props.image} alt='project image' height='100px'/>
+            <img src={props.image} alt='project' height='100px'/>
             {props.children}
         </a>
     );
