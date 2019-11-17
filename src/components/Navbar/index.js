6de0a87b1b7b3a50294932281f5396bcc5ec42a9
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-11-17 17:07:13 +0100

    restructure navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,44 +3,43 @@ import './index.css';
 import PureLink from '../PureLink';
 import {routes} from '../../routes';
 
+function getLinks() {
+    return routes.filter(route => route.display).map(route => {
+        return (
+            <PureLink key={route.id} to={route.path}>
+                <route.nav>{route.name}</route.nav>
+            </PureLink>
+        );
+    });
+}
+
 export default function Navbar(props) {
     const path = window.location.pathname;
+    const links = getLinks();
 
     const crossLink =
         <PureLink to='/'>
             <img src='/assets/cross.svg' height='20' alt='Cross'/>
         </PureLink>;
 
-    const links = routes.filter(route => route.display).map(route => {
-        return <PureLink key={route.id} to={route.path}>
-            <route.nav>{route.name}</route.nav>
-        </PureLink>;
-
-    });
 
     if (path === '/') {
         return (
             <CommonNavbar>
-                <div className='navbar-menu d-none d-lg-flex w-100 flex-row-reverse align-items-center'>
+                <div className='d-none d-lg-flex w-100 flex-row-reverse align-items-center'>
                     {links}
                 </div>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                <button className="navbar-toggler d-block d-lg-none" type="button" data-toggle="collapse"
                         data-target="#middleMenu" aria-controls="middleMenu"
                         aria-expanded="false"
                         aria-label="Toggle navigation">
                     <img src='./assets/burger.svg' height='20' alt='Hamburger menu'/>
                 </button>
-                <div id='middleMenu' className='collapse w-100 d-lg-none'>
-                    <div className='d-flex flex-column-reverse justify-content-center align-items-center'>
-                        {links}
-                    </div>
-                </div>
             </CommonNavbar>
         );
     } else {
         return (
             <CommonNavbar>
-                <div className='navbar-menu d-none d-lg-flex w-100'/>
                 {crossLink}
             </CommonNavbar>
         );
@@ -48,15 +47,23 @@ export default function Navbar(props) {
 };
 
 function CommonNavbar(props) {
+    const links = getLinks();
     return (
-        <div className='col-lg-10 m-lg-auto pt-4 d-flex navbar navbar-expand-lg'>
-            <div className='navbar-brand'>
-                <PureLink to='/'>
-                    <img src='./assets/logo.svg' height='50'
-                         alt='Laszlo Borbely | MSc Student, Front-end & Back-end Developer'/>
-                </PureLink>
+        <>
+            <div className='col-12 col-lg-10 m-auto pt-4 d-flex justify-content-between align-items-center'>
+                <div className='navbar-brand'>
+                    <PureLink to='/'>
+                        <img src='./assets/logo.svg' height='50'
+                             alt='Laszlo Borbely | MSc Student, Front-end & Back-end Developer'/>
+                    </PureLink>
+                </div>
+                {props.children}
             </div>
-            {props.children}
-        </div>
+            <div id='middleMenu' className='collapse w-100 d-lg-none'>
+                <div className='d-flex flex-column-reverse justify-content-center align-items-center'>
+                    {links}
+                </div>
+            </div>
+        </>
     );
 }
