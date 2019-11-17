9396f1c1d22361fe7529f0fd33c1af41cd7f218d
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-11-17 17:13:42 +0100

    contact view styling change

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -7,7 +7,7 @@ export default function Contact(props) {
         <Frame>
             <h1>Thanks for taking the time to reach out. </h1>
             <h2>Feel free to send me a mail.</h2>
-            <div className='d-flex justify-content-center mt-5 pt-5'>
+            <div className='d-flex justify-content-center mt-5'>
                 <a href='mailto:laszlo.borbely@rwth-aachen.de'><PureButton>laszlo.borbely@rwth-aachen.de</PureButton></a>
             </div>
         </Frame>
