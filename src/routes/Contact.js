02844ebde972a976c196a6665831b58683fa5314
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-11-17 01:37:40 +0100

    add contact view content

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import Frame from '../components/Frame';
+import PureButton from '../components/PureButton';
 
 export default function Contact(props) {
     return (
         <Frame>
-            <div>Contact</div>
+            <h1>Thanks for taking the time to reach out. </h1>
+            <h2>Feel free to send me a mail.</h2>
+            <div className='d-flex justify-content-center mt-5 pt-5'>
+                <a href='mailto:laszlo.borbely@rwth-aachen.de'><PureButton>laszlo.borbely@rwth-aachen.de</PureButton></a>
+            </div>
         </Frame>
     );
 };
