ba7c568856792ea3f0d9ca1a3cd0a5dd0f8739cf
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-11-17 01:36:47 +0100

    button styling & onclick action

--- src/components/PureButton/index.js
+++ src/components/PureButton/index.js
@@ -3,7 +3,7 @@ import './index.css';
 
 export default function PureButton(props) {
     return (
-        <div className='button-link'>
+        <div className='button-link' onClick={props.onClick}>
             {props.children}
         </div>
     );
