0bc5ab617467158e007a590f04ce59e327a360af
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2019-12-02 14:56:53 +0100

    remove unused css classes

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -4,8 +4,8 @@ import './index.css';
 export default function ProjectCard(props) {
     return (
         <a href={props.link} className='project-card d-flex flex-row align-items-center p-2'>
-            <img className='flex-1 project-image' src={props.image} alt='project'/>
-            <div className='flex-1 d-flex flex-column justify-content-start ml-3'>
+            <img className='project-image' src={props.image} alt='project'/>
+            <div className='d-flex flex-column justify-content-start ml-3'>
                 <div className='project-name'>{props.name}</div>
                 <div className='project-description'>{props.description}</div>
             </div>
