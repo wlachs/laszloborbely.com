3174a6e48e2dcbcaf9cf2d373638ebb6e5c804c6
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-01-27 11:25:54 +0100

    update literature wizard access link from local dev to prod

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -8,9 +8,12 @@ export default function Projects() {
             <h1>My recent work</h1>
             <h2>Check out my latest projects.</h2>
             <div className='d-flex justify-content-center mt-5'>
-                <ProjectCard link='https://literature.laszloborbely.com' image='./assets/literature.png'
-                             description='A lightweight tool for managing scientific publications.'
-                             name='Literature Wizard'/>
+                <ProjectCard
+                    link='https://swofi.net:30012'
+                    image='./assets/literature.png'
+                    description='A lightweight tool for managing scientific publications.'
+                    name='Literature Wizard'
+                />
             </div>
         </Frame>
     );
