b9076124c1bfa0b27b5518cb549529f2c102c858
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-08 14:47:58 +0100

    conditional link generation in PureButton component

--- src/components/PureButton/index.js
+++ src/components/PureButton/index.js
@@ -3,9 +3,17 @@ import './index.css';
 import {Link} from 'react-router-dom';
 
 export default function PureButton(props) {
-    return (
-        <Link to={props.to} className='button-link link' onClick={props.onClick}>
-            {props.children}
-        </Link>
-    );
+    if (props.to) {
+        return (
+            <Link to={props.to} className='button-link link' onClick={props.onClick}>
+                {props.children}
+            </Link>
+        );
+    } else {
+        return (
+            <div className='button-link link' onClick={props.onClick}>
+                {props.children}
+            </div>
+        );
+    }
 };
