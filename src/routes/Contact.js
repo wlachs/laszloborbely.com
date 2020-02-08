ea4243b4ffba63ff59e31a5dacce57c43bd08a34
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-08 14:35:05 +0100

    update contact email address

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -8,7 +8,7 @@ export default function Contact(props) {
             <h1>Thanks for taking the time to reach out. </h1>
             <h2>Feel free to send me a mail.</h2>
             <div className='d-flex justify-content-center mt-5'>
-                <a href='mailto:laszlo.borbely@rwth-aachen.de'><PureButton>laszlo.borbely@rwth-aachen.de</PureButton></a>
+                <a href='mailto:hello@laszloborbely.com'><PureButton>hello@laszloborbely.com</PureButton></a>
             </div>
         </Frame>
     );
