c9caf9746e220c4c2195aa16da4e5c7e82f24e88
László Borbély <laci.dj@gmail.com>
2020-09-30 13:19:16 +0200

    project page restyle

--- client/src/components/ProjectCard/index.jsx
+++ client/src/components/ProjectCard/index.jsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import './index.css';
 
-export default function ProjectCard(props) {
-    return (
-        <a href={props.link} className='project-card d-flex flex-row align-items-center p-2'>
-            <img className='project-image' src={props.image} alt='project'/>
-            <div className='d-flex flex-column justify-content-start ml-3'>
-                <div className='project-name'>{props.name}</div>
-                <div className='project-description'>{props.description}</div>
-            </div>
-        </a>
-    );
+function ProjectCard(props) {
+  const {
+    link, image, name, description,
+  } = props;
+
+  return (
+    <a href={link} className="project-card d-flex flex-row align-items-center p-2 mb-1">
+      <img className="project-image" src={image} alt="project" />
+      <div className="d-flex flex-column justify-content-start ml-3">
+        <div className="project-name">{name}</div>
+        <div className="project-description">{description}</div>
+      </div>
+    </a>
+  );
+}
+
+ProjectCard.propTypes = {
+  link: PropTypes.string,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
+
+ProjectCard.defaultProps = {
+  link: '#',
+};
+
+export default ProjectCard;
--- client/src/routes/Projects.jsx
+++ client/src/routes/Projects.jsx
@@ -5,8 +5,14 @@ export default function Projects() {
   return (
     <>
       <h1>My recent work</h1>
-      <h2>Check out my latest projects.</h2>
-      <div className="d-flex justify-content-center mt-5">
+      <h2>Check out my latest contributions.</h2>
+      <div className="d-flex align-items-center mt-5 flex-column">
+        <ProjectCard
+          link="https://swofi.net"
+          image="./assets/swofi.png"
+          description="Scientific Workflow Guide."
+          name="SWOFI"
+        />
         <ProjectCard
           link="https://swofi.net:30012"
           image="./assets/literature.png"
