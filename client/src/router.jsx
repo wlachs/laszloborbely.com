a09f43b6d964a392f36c122f9b6c2ad05d197991
László Borbély <laci.dj@gmail.com>
2020-09-30 12:30:40 +0200

    remove graphql connection for now

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,42 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route as ReactRoute } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
 import routes from './routes';
 import Frame from './components/Frame';
-import { PAGE_TITLE } from './apollo/queries/config';
-import Loading from './routes/Loading';
-import Error from './routes/Error';
 
 function Router() {
-  const { loading, error, data } = useQuery(PAGE_TITLE);
-
-  let FinalRoute;
-  if (error) {
-    FinalRoute = () => <Error />;
-  } else if (loading) {
-    FinalRoute = () => <Loading />;
-  } else {
-    const routeMap = routes.map((Route) => (
-      <ReactRoute key={Route.id} path={Route.path}>
-        {Route.View}
-      </ReactRoute>
-    ));
-
-    FinalRoute = () => (
-      <Switch>
-        {routeMap}
-      </Switch>
-    );
-  }
-
-  if (data) {
-    document.title = data.config.pageTitle;
-  }
+  const routeMap = routes.map((Route) => (
+    <ReactRoute key={Route.id} path={Route.path}>
+      {Route.View}
+    </ReactRoute>
+  ));
 
   return (
     <BrowserRouter>
       <Frame>
-        <FinalRoute />
+        <Switch>
+          {routeMap}
+        </Switch>
       </Frame>
     </BrowserRouter>
   );
