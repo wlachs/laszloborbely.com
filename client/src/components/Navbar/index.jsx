af5d03a4523b311e43d7a55a880adb3683edcfeb
László Borbély <laci.dj@gmail.com>
2020-10-01 19:26:30 +0200

    mobile menu fix

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './index.css';
 import PureLink from '../PureLink';
 import Links from '../../routes/Links';
 
 function Navbar() {
+  const [isExpanded, expand] = useState(false);
+
   return (
-    <CommonNavbar>
+    <CommonNavbar isExpanded={isExpanded}>
       <div className="d-none d-lg-flex w-100 flex-row-reverse align-items-center">
         <Links />
       </div>
       <button
         className="navbar-toggler d-block d-lg-none"
         type="button"
-        data-toggle="collapse"
-        data-target="#middleMenu"
-        aria-controls="middleMenu"
-        aria-expanded="false"
+        onClick={() => expand(!isExpanded)}
         aria-label="Toggle navigation"
       >
         <img src="/assets/burger.svg" height="20" alt="Hamburger menu" />
@@ -25,8 +24,35 @@ function Navbar() {
   );
 }
 
+function MiddleMenu(props) {
+  const { children, isExpanded } = props;
+  if (isExpanded) {
+    return (
+      <div className="w-100 d-lg-none">
+        {children}
+      </div>
+    );
+  }
+  return (
+    <div className="collapse w-100 d-lg-none">
+      {children}
+    </div>
+  );
+}
+
+MiddleMenu.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element),
+  isExpanded: PropTypes.bool,
+};
+
+MiddleMenu.defaultProps = {
+  children: null,
+  isExpanded: false,
+};
+
 function CommonNavbar(props) {
-  const { children } = props;
+  const { children, isExpanded } = props;
+
   return (
     <>
       <div className="col-12 col-lg-10 m-auto pt-4 d-flex justify-content-between align-items-center">
@@ -40,21 +66,23 @@ function CommonNavbar(props) {
         </PureLink>
         {children}
       </div>
-      <div id="middleMenu" className="collapse w-100 d-lg-none">
+      <MiddleMenu isExpanded={isExpanded}>
         <div className="d-flex flex-column-reverse justify-content-center align-items-center">
           <Links />
         </div>
-      </div>
+      </MiddleMenu>
     </>
   );
 }
 
 CommonNavbar.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element),
+  isExpanded: PropTypes.bool,
 };
 
 CommonNavbar.defaultProps = {
   children: null,
+  isExpanded: false,
 };
 
 export default Navbar;
