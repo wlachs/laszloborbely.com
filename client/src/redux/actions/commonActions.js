d95f2f1053f7a9b66088b72bdaf19e2165ae3de6
László Borbély <laci.dj@gmail.com>
2020-10-11 20:14:15 +0200

    implement redux store structure with actions and reducers

---
+++ client/src/redux/actions/commonActions.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import {
+  REQUEST_API_TOKEN_ERROR,
+  REQUEST_API_TOKEN_START,
+  REQUEST_API_TOKEN_SUCCESS,
+} from '../actionTypes/commonActionTypes';
+import getCurrentConfiguration, { authEndpoint } from '../../config';
+
+export function sendTokenRequest(email, password) {
+  return (dispatch) => {
+    dispatch({
+      type: REQUEST_API_TOKEN_START,
+    });
+
+    const configuration = getCurrentConfiguration();
+    const requestUri = configuration.serverUri + authEndpoint;
+
+    axios.post(requestUri, {
+      email,
+      password,
+    })
+      .then((response) => {
+        if (response.status === 202) {
+          dispatch({
+            type: REQUEST_API_TOKEN_SUCCESS,
+            value: response.data.token,
+          });
+        } else {
+          dispatch({
+            type: REQUEST_API_TOKEN_ERROR,
+            value: 'Incorrect username or password!',
+          });
+        }
+      })
+      .catch(() => {
+        dispatch({
+          type: REQUEST_API_TOKEN_ERROR,
+          value: 'An error occurred, please try again!',
+        });
+      });
+  };
+}
