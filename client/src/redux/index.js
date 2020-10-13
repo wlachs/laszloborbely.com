88b78d8c010d1510ff5f97bc17c7735f137a8a9c
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-10-13 13:31:28 +0200

    remove redux devtools extension

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -27,7 +27,5 @@ export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
