5b4af42c601a3ad41e46d32ac96e396806dd7d4b
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-06-30 22:16:29 +0200

    syntax highlighting

--- client/src/components/MarkdownRenderer/index.tsx
+++ client/src/components/MarkdownRenderer/index.tsx
@@ -1,12 +1,29 @@
 import './index.css';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 
 function MarkdownRenderer({content}: {
 	content: string;
 }) {
 	return (
-		<ReactMarkdown className='d-flex flex-column markdown col-11 col-lg-12' remarkPlugins={[remarkGfm]}>
+		<ReactMarkdown className='d-flex flex-column markdown col-11 col-lg-12'
+			remarkPlugins={[remarkGfm]}
+			components={{
+				code({node, inline, className, children, ...props}) {
+					const match = /language-(\w+)/.exec(className ?? '');
+					return !inline && match ? (
+						<SyntaxHighlighter
+							language={match[1]}>
+							{String(children).replace(/\n$/, '')}
+						</SyntaxHighlighter>
+					) : (
+						<code {...props} className={className}>
+							{children}
+						</code>
+					);
+				},
+			}}>
 			{content}
 		</ReactMarkdown>
 	);
