c63ff59dfccba2ba4e78b0a6deec967a8c5086d3
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-08-26 23:54:34 +0200

    remove responsive middle menu

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,79 +1,38 @@
-import {type ReactNode, useState} from 'react';
+import {type ReactNode} from 'react';
 import './index.css';
 import Links from '../Links/index.jsx';
 import PureLinkHighlight from '../PureLinkHighlight/index.jsx';
 
 // Assets
-import burger from '../../assets/burger.svg';
 import logo from '../../assets/logo.svg';
 
 function Navbar() {
-	const [isExpanded, expand] = useState(false);
-
 	return (
-		<CommonNavbar isExpanded={isExpanded}>
-			<div className='d-none d-lg-flex w-100 flex-row-reverse align-items-center'>
+		<CommonNavbar>
+			<div className='d-flex w-100 flex-row-reverse align-items-center'>
 				<Links />
 			</div>
-			<button
-				className='navbar-toggler d-block d-lg-none'
-				type='button'
-				onClick={() => {
-					expand(!isExpanded);
-				}}
-				aria-label='Toggle navigation'
-			>
-				<img src={burger} height='20' alt='Hamburger menu' />
-			</button>
 		</CommonNavbar>
-	);
-}
-
-function MiddleMenu(props: {
-	children?: ReactNode;
-	isExpanded?: boolean;
-}) {
-	const {children, isExpanded} = props;
-	if (isExpanded) {
-		return (
-			<div className='w-100 d-lg-none mb-5'>
-				{children}
-			</div>
-		);
-	}
-
-	return (
-		<div className='collapse w-100 d-lg-none'>
-			{children}
-		</div>
 	);
 }
 
 function CommonNavbar(props: {
 	children?: ReactNode;
-	isExpanded?: boolean;
 }) {
-	const {children, isExpanded} = props;
+	const {children} = props;
 
 	return (
-		<>
-			<div className='col-12 col-lg-10 px-1 w-100 mb-lg-5 px-5 d-flex justify-content-between align-items-center'>
-				<PureLinkHighlight to='/'>
-					<img
-						src={logo}
-						height='40'
-						className='navbar-logo'
-						alt='László Borbély - Full-Stack Software Engineer'
-					/>
-				</PureLinkHighlight>
-				{children}
-			</div>
-			<MiddleMenu isExpanded={isExpanded}>
-				<div className='d-flex flex-column-reverse justify-content-center align-items-center'>
-					<Links />
-				</div>
-			</MiddleMenu>
-		</>
+		<div className='col-12 col-lg-10 px-1 w-100 mb-lg-5 px-5 d-flex justify-content-between align-items-center'>
+			<PureLinkHighlight to='/'>
+				<img
+					src={logo}
+					height='40'
+					className='navbar-logo'
+					alt='László Borbély - Full-Stack Software Engineer'
+				/>
+			</PureLinkHighlight>
+			{children}
+		</div>
 	);
 }
 
