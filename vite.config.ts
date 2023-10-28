534f258b9ee93417de184304628793c1c33c8438
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-10-28 23:12:08 +0200

    allow direct live API connection during development

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,35 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			'/api/blog': {
-				target: 'http://localhost:8080',
-				changeOrigin: true,
-				rewrite: path => path.replace(/^\/api\/blog/, ''),
-			},
-		},
-	},
+export default defineConfig(({mode}) => {
+	switch (mode) {
+		case 'live':
+			return {
+				plugins: [react()],
+				server: {
+					proxy: {
+						'/api/blog': {
+							target: 'https://laszloborbely.com',
+							changeOrigin: true,
+							secure: false,
+						},
+					},
+				},
+			};
+		default:
+			return {
+				plugins: [react()],
+				server: {
+					proxy: {
+						'/api/blog': {
+							target: 'http://localhost:8080',
+							changeOrigin: true,
+							rewrite: path => path.replace(/^\/api\/blog/, ''),
+						},
+					},
+				},
+			};
+	}
 });
