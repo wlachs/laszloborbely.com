1b44bbdb6ab3f5189bac451440a663244286f9c4
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-11-05 22:47:34 +0100

    split production build into chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,55 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import react from '@vitejs/plugin-react-swc';
-import {type ConfigEnv, defineConfig, type UserConfig} from 'vite';
+import {type ManualChunksOption} from 'rollup';
+import {visualizer} from 'rollup-plugin-visualizer';
+import {type ConfigEnv, defineConfig, type ProxyOptions, splitVendorChunkPlugin, type UserConfig} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}: ConfigEnv): UserConfig => {
+export default defineConfig((env: ConfigEnv): UserConfig => ({
+	plugins: [react(), splitVendorChunkPlugin(), visualizer()],
+	server: {
+		proxy: proxyOptions(env),
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: manualChunksOptions,
+			},
+		},
+	},
+}));
+
+const proxyOptions = ({mode}: ConfigEnv): Record<string, ProxyOptions> => {
 	if (mode === 'live') {
 		return {
-			plugins: [react()],
-			server: {
-				proxy: {
-					'/api/blog': {
-						target: 'https://laszloborbely.com',
-						changeOrigin: true,
-						secure: false,
-					},
-					'/static': {
-						target: 'https://laszloborbely.com',
-						changeOrigin: true,
-						secure: false,
-					},
-				},
+			'/api/blog': {
+				target: 'https://laszloborbely.com',
+				changeOrigin: true,
+				secure: false,
+			},
+			'/static': {
+				target: 'https://laszloborbely.com',
+				changeOrigin: true,
+				secure: false,
 			},
 		};
 	}
 
 	return {
-		plugins: [react()],
-		server: {
-			proxy: {
-				'/api/blog': {
-					target: 'http://localhost:8080',
-					changeOrigin: true,
-					rewrite: path => path.replace(/^\/api\/blog/, ''),
-				},
-			},
+		'/api/blog': {
+			target: 'http://localhost:8080',
+			changeOrigin: true,
+			rewrite: path => path.replace(/^\/api\/blog/, ''),
 		},
 	};
-});
+};
+
+const manualChunksOptions: ManualChunksOption = (id: string) => {
+	if (id.includes('moment')) {
+		return 'moment';
+	}
+
+	if (id.includes('refractor')) {
+		return 'refractor';
+	}
+};
