41f5dbeb21d7be7fc20cdb5fe53bde643b736859
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-04 17:51:05 +0100

    change name and update dependencies

--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -28,7 +28,7 @@ function Blog() {
 				</Helmet>
 				{/* Page content */}
 				<ContentRow>
-					<h1>László&apos;s dev-blog</h1>
+					<h1>László Borbély</h1>
 					<p>
                         Feel free to check out my latest posts. Who knows, perhaps you&apos;ll find something interesting.
 					</p>
