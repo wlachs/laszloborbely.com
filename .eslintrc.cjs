7ef28c1d2da5d354ed214e33ae3cdea4e78c376a
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-02-05 01:08:02 +0100

    eslint import sorting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,37 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: ['plugin:react/recommended', 'prettier'],
+	extends: [
+		'plugin:react/recommended',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+		'plugin:@tanstack/eslint-plugin-query/recommended',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	plugins: ['react'],
+	plugins: ['react', 'import', 'simple-import-sort'],
 	rules: {
 		'react/react-in-jsx-scope': 'off',
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+		'import/first': 'error',
+		'import/newline-after-import': 'error',
+		'import/no-duplicates': 'error',
 	},
 	settings: {
 		react: {
 			version: 'detect',
+		},
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx'],
+		},
+		'import/resolver': {
+			typescript: true,
+			node: true,
 		},
 	},
 };
